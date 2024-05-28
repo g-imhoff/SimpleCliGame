c46243a5b581d0cd6f5186258dd5f659e06515a5
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-05-28 20:10:34 +0200

    fin de pos pour le moment

--- src/game/tools/Pos.java
+++ src/game/tools/Pos.java
@@ -1,4 +1,22 @@
 package game.tools;
 
 public class Pos {
+    private int x, y;
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
