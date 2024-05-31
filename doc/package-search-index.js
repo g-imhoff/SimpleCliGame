9b5f2c130c1d44e2a5c01d5d9df2cd53f36bcaf1
Guillaume IMHOFF <guillaume.imhoff170804@gmail.com>
2024-05-31 17:15:00 +0200

    fin du projet

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,5 +1,1 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "u": "allpackages-index.html"
-}, {"l": "game"}, {"l": "game.level"}, {"l": "game.level.chooseLevel"}, {"l": "game.level.entities"}, {"l": "game.level.generateLevel"}, {"l": "game.player"}, {"l": "game.tools"}, {"l": "game.tools.aStar"}];
-updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"game"},{"l":"game.level"},{"l":"game.level.chooseLevel"},{"l":"game.level.entities"},{"l":"game.level.generateLevel"},{"l":"game.player"},{"l":"game.tools"},{"l":"game.tools.aStar"}];updateSearchResults();
--- src/game/player/Player.java
+++ src/game/player/Player.java
@@ -16,7 +16,7 @@ public class Player {
     /**
      * p, the player position
      */
-    Pos p;
+    private Pos p;
 
     /**
      * Constructor
